d9947b486cdb030f0183b27bae7ce28949e644b3
Cheol wook <dlcjfdnr789@naver.com>
2025-05-08 14:07:31 +0900

    feature - header scroll fix 기능 추가, 최대,최소값 추가,  box-shadow 조정, main 페이지 수정, comm a 태그 color 부모 상속

--- script/components/header.js
+++ script/components/header.js
@@ -21,6 +21,7 @@ class JeanHeader extends HTMLElement {
 
       // header 태그 생성 및 내용 삽입
       const header = document.createElement('header');
+      header.id = 'header';
       header.innerHTML = `
         <div class="content-center">
           <h1 class="logo"><a href="#">청바지</a></h1>
@@ -44,9 +45,24 @@ class JeanHeader extends HTMLElement {
       // Shadow DOM에 style과 header 삽입
       this.shadow.appendChild(style);
       this.shadow.appendChild(header);
+
+      // scroll 이벤트 등록 (render 완료 후)
+      this.setupScrollListener();
     } catch (error) {
       console.error('JeanHeader 로딩 실패:', error);
     }
+  }
+
+  // scroll 에 따라 fix 이벤트
+  setupScrollListener() {
+    const nav = this.shadow.querySelector('#header');
+    if (!nav) return;
+
+    document.addEventListener('scroll', () => {
+      const sPos = document.documentElement.scrollTop;
+      if (sPos > 50) nav.classList.add('fixed');
+      else nav.classList.remove('fixed');
+    });
   }
 }
 

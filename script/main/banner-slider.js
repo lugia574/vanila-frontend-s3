a8cd112bab3c78cfca7977dde28889a67316767e
Cheol wook <dlcjfdnr789@naver.com>
2025-05-14 11:30:59 +0900

    feat : license card  슬라이드 추가

--- script/main/banner-slider.js
+++ script/main/banner-slider.js
@@ -1,3 +1,4 @@
+// 메인배너 text 슬라이드
 const textSlider = new Swiper('.text-slider', {
   slidesPerView: 1,
   effect: 'fade',
@@ -46,6 +47,7 @@ const textSlider = new Swiper('.text-slider', {
   },
 });
 
+// 메인 배너 이미지 슬라이드
 const imageSlider = new Swiper('.image-slider', {
   slidesPerView: 1,
   loop: true,
@@ -53,6 +55,7 @@ const imageSlider = new Swiper('.image-slider', {
 
 textSlider.controller.control = imageSlider;
 
+// 공모전 슬라이드
 const contestSlider = new Swiper('.contest-swiper', {
   spaceBetween: 16,
   speed: 500,
@@ -60,6 +63,36 @@ const contestSlider = new Swiper('.contest-swiper', {
   navigation: {
     nextEl: '.contest-button-next',
     prevEl: '.contest-button-prev',
+  },
+  breakpoints: {
+    // 모바일 (0~767px)
+    0: {
+      slidesPerView: 1,
+      slidesPerGroup: 1,
+    },
+    480: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+    },
+    768: {
+      slidesPerView: 3,
+      slidesPerGroup: 1,
+    },
+    1024: {
+      slidesPerView: 4,
+      slidesPerGroup: 1,
+    },
+  },
+});
+
+// 자격증 슬라이드
+const licenseSlider = new Swiper('.license-swiper', {
+  spaceBetween: 16,
+  speed: 500,
+  loop: true,
+  navigation: {
+    nextEl: '.license-button-next',
+    prevEl: '.license-button-prev',
   },
   breakpoints: {
     // 모바일 (0~767px)

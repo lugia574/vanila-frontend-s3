bc3f1a530dab46b4e93688a9d9555e7e6e4a52d0
Kim Hyemi <qnlqlaqkq120@gmail.com>
2025-05-19 22:30:54 +0900

    feat: 커뮤니티 리스트 필터 기능 추가

--- script/community/community-list.js
+++ script/community/community-list.js
@@ -1,8 +1,4 @@
 
-// 필터
-const filterBtns = document.querySelectorAll(".filter-wrap a");
-// 검색 조건
-const searchConditions = document.querySelector(".search-conditions");
 // 필터 초기화
 const filterReset = document.querySelector(".filter-reset");
 // 글작성 버튼
@@ -16,68 +12,6 @@ const divisionTypes = document.querySelectorAll(".division-type li a");
 // content 
 const contentLinks = document.querySelectorAll(".content-link");
 
-
-// 필터 mouse over 이벤트 
-// filterBtns.forEach(btn => {
-//   btn.addEventListener('mouseover', () => {
-//     btn.classList.add("btn-active");
-//   });
-
-//   btn.addEventListener('mouseout', () => {
-//     btn.classList.remove("btn-active");
-//   });
-
-// });
-
-
-// 필터 클릭시 이벤트(style)
-// TODO : 실제 적용 필요
-filterBtns.forEach(btn => {
-  btn.addEventListener("click", () => {
-    const filterName = btn.textContent.trim();
-    // 필터 중복 방지
-    const matchedFilter = Array.from(searchConditions.children).find(
-      child => child.textContent.trim() === filterName
-    );
-
-    if (btn.classList.contains("btn-active")) {
-      btn.classList.remove("btn-active");
-
-      if (matchedFilter) {
-        searchConditions.childNodes.forEach(filter => {
-          if (filter.textContent === filterName) {
-            filter.remove();
-          }
-        });
-      }
-    } else {
-      btn.classList.add("btn-active");
-
-      if (!matchedFilter) {
-        const liTag = document.createElement("li");
-        const aTag = document.createElement("a");
-
-        aTag.textContent = filterName;
-        aTag.classList.add("search-filter-active");
-        aTag.href = "#";
-
-        liTag.appendChild(aTag);
-        searchConditions.appendChild(liTag);
-
-        // 동적으로 생성된 liTag에 이벤트가 발생한 경우
-        liTag.addEventListener("click", () => {
-          filterBtns.forEach(filter => {
-            const aTagName = liTag.querySelector("a").textContent.trim();
-            if (aTagName === filter.textContent.trim()) {
-              filter.classList.toggle("btn-active");
-            }
-          });
-          liTag.remove();
-        });
-      }
-    }
-  });
-});
 
 // 초기화
 filterReset.addEventListener("click", () => {
